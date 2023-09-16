afb2bfaf9bc65df8df89059dcbb85243d2df0375
Joaquín Elía <joaquin.elia@hotmail.com>
2023-09-15 23:59:49 -0300

    Testimonials carousel

--- js/index.js
+++ js/index.js
@@ -18,6 +18,16 @@ window.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', closeMenu)
     });
     
+    window.addEventListener('scroll', () => {
+        const whatsappButton = document.getElementById('whatsappButton');
+        if (window.pageYOffset > 400) { // Ajusta este valor según tus necesidades
+            whatsappButton.style.display = 'flex';
+        } else {
+            whatsappButton.style.display = 'none';
+        }
+    });
+
+
     // calculators
     const showTraffic = document.getElementById('showTraffic');
     const showWork = document.getElementById('showWork');
@@ -83,7 +93,7 @@ window.addEventListener('DOMContentLoaded', () => {
         
         const compensation = Math.round(a * (1 - Vn) * (1 / i));
 
-        result.textContent = `La indemnización es de: $${compensation.toLocaleString('es-AR')} pesos.`;
+        result.innerHTML = `La indemnización es de: <strong style="font-size: 18px;">$${compensation.toLocaleString('es-AR')}</strong> pesos.`;
     });
     
     // work accident
@@ -122,7 +132,7 @@ window.addEventListener('DOMContentLoaded', () => {
     
         accidentPlace === "work" && (compensation *= 1.2)
         
-        resultWork.textContent = `La indemnización es de: $${compensation.toLocaleString('es-AR')} pesos.`;
+        resultWork.innerHTML = `La indemnización es de: <strong style="font-size: 18px;">$${compensation.toLocaleString('es-AR')}</strong> pesos.`;
     });
     
     // dismissal 
@@ -213,6 +223,184 @@ window.addEventListener('DOMContentLoaded', () => {
         const totalIndemnization = Math.round(indemnizacionAntiguedad + indemnizacionPreaviso + vacationValue + proportionalVacationValue + aguinaldoValue + workedDaysValue);
         
         // Mostrar el resultado en la página
-        document.querySelector("#resultDismissal").textContent = `La indemnización es de: ${totalIndemnization.toLocaleString('es-AR')} pesos.`;
+        document.querySelector("#resultDismissal").innerHTML = `La indemnización es de: <strong style="font-size: 18px;">$${totalIndemnization.toLocaleString('es-AR')}</strong> pesos.`;
     });
+
+
+
+    // testimonials
+    const slider = document.querySelector('.testimonials');
+    const testimonialsContainer = document.getElementById('testimonialsContainer');
+
+    const testimonials = [
+        {
+            img: "https://lh3.googleusercontent.com/a/ACg8ocJV4pW8GJDFX6yAgZXcJPEcXeUzjwqeJHGCBNGigff-=s64-c-rp-mo-br100",
+            name: "Leandro Barrena",
+            quote: "Gente con carisma, responsables y muy dedicados a su labor un lujo en pocas palabras",
+            link: "https://g.co/kgs/kb7joo"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a-/ALV-UjVLcGdojRApTliS157D-YEW0w-U-pPH_w8qPQCCXDcQw2Q=s64-c-rp-mo-br100",
+            name: "Nazareno Barraza",
+            quote: "Excelente atención. Amabilidad y cordialidad, totalmente satisfecho.",
+            link: "https://g.co/kgs/2Fi24c"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a/ACg8ocLdm0pAY8_JL7FB9GxxDPPMXwCbczVvpA8lzxYISvax=s64-c-rp-mo-br100",
+            name: "Alfredo Vivarelli",
+            quote: "Grandes profesionales y mejores personas. Comprometidos y eficaces. Para contar con el estudio.",
+            link: "https://g.co/kgs/MtZf6f"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a-/ALV-UjWz_H16vdbSIgXkefGn9zZbfnrZbvrsp00tYx3vCzpW4x4F=s64-c-rp-mo-br100",
+            name: "Mariana Acosta",
+            quote: "Excelente abogado muy recomendable respecto a la defensa en materia sumario muy conforme con su amabilidad y excelencia",
+            link: "https://g.co/kgs/gHYgxg"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a/ACg8ocKHGDzYEAl1OTdl0hObKZPRuPtuDDtl_RsiyCQJ4EEu=s64-c-rp-mo-br100",
+            name: "ignacio eyherabide",
+            quote: "Exelentes profesionales muy buena atención amabilidad y muy cumplidores",
+            link: "https://g.co/kgs/K7WDcE"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a-/ALV-UjUZZvtNlA7ILaZD7STbl-isovNE813mwSS2987KdEd_UJ8=s64-c-rp-mo-br100",
+            name: "Yohana Jaureguiberry",
+            quote: "Muy buen servicio y atencion, manejo responsable y profesional!",
+            link: "https://g.co/kgs/MoQT4X"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a/ACg8ocLdn9YsipvTyOKDUNrfj2wwU6tuuDD8hlh2Hsi7ozWQ=s64-c-rp-mo-br100",
+            name: "Marcela Jose",
+            quote: "Excelente estudio y pronta atención y solución a su clientela. Súper recomendable!!!",
+            link: "https://g.co/kgs/JvDiLZ"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a/ACg8ocL-8SFzuPrNtdJqFNeYIYZWxxM06ZTdxUbpP2RZ1nei=s64-c-rp-mo-br100",
+            name: "Enrique Bautista",
+            quote: "Dedicación profesional en cada expediente, buena atención, dispuestos y comprometidos en cada dificultad. Atención excelente.",
+            link: "https://g.co/kgs/ysVgjm"
+        },
+        {
+            img: "https://lh3.googleusercontent.com/a-/ALV-UjWV7z10r5U-oMPXvmPYeYopWPrcDQkId8JWOPjJIvt_e2o=s64-c-rp-mo-br100",
+            name: "Lola Elizari",
+            quote: "Excelentes profesionales y mucha calidad humana. Super recomendables!",
+            link: "https://g.co/kgs/uZcYZe"
+        },
+    ]
+
+    testimonials.forEach(({img, name, quote, link}) => {
+        const cardHTML = `
+            <article class="testimonial-card">
+                <header class="testimonial-header">
+                    <div class="testimonial-header-client">
+                        <img src="${img}" alt="Foto de ${name}" class="testimonial-img">
+                        <h3 class="testimonial-name">${name}</h3>
+                    </div>
+                    <div class="testimonial-stars"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                    </div>
+                </header>
+                <blockquote cite="${name}" class="testimonial-quote">
+                    ${quote}
+                </blockquote>
+                <footer class="testimonial-footer">
+                    <img src="../assets/googleIcon.svg" class="google-icon" alt="Logo de Google en referencia a testimonios">
+                    <a  href="${link}"
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                    > Ver comentario
+                    </a>
+                </footer>
+            </article>
+        `;
+
+        testimonialsContainer.innerHTML += cardHTML;
+    });
+    // carousel testimonials
+    const firstCard = slider.querySelectorAll('article')[0];
+    const arrowIcons = document.querySelectorAll('.arrow-slider')
+
+    let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+    
+    const showHideIcons = () => {
+        let scrollWidth = slider.scrollWidth - slider.clientWidth; // getting max scorll width
+        arrowIcons[0].style.display = slider.scrollLeft === 0 ? 'none' : 'block'
+        arrowIcons[1].style.display = slider.scrollLeft === scrollWidth ? 'none' : 'block'
+    }
+    
+    arrowIcons.forEach(icon => {
+        icon.addEventListener("click", () => {
+            let firstCardWidth = firstCard.clientWidth + 16; // getting first card
+            slider.scrollLeft += icon.id === 'iconLeft' ? -firstCardWidth : firstCardWidth;
+            showHideIcons()
+        })
+    })
+
+    const autoSlide = () => {
+        // if there's no img left to scroll then return from here
+        if(slider.scrollLeft - (slider.scrollWidth - slider.clientWidth) > -1 || slider.scrollLeft <= 0) return;
+
+        positionDiff = Math.abs(positionDiff) // positionDiff value to positive
+        let firstCardWidth = firstCard.clientWidth + 8;
+        let valDifference = firstCardWidth - positionDiff;
+        
+        if(slider.scrollLeft > prevScrollLeft) {// user scrolling to the right
+            return slider.scrollLeft += positionDiff > firstCardWidth / 2 ? valDifference : -positionDiff;
+        }
+        // user scrolling to the left
+        slider.scrollLeft -= positionDiff > firstCardWidth / 2 ? valDifference : -positionDiff;
+    }
+
+    const dragStart = (e) => {
+        isDragStart = true;
+        prevPageX = e.pageX || e.touches[0].pageX;
+        prevScrollLeft = slider.scrollLeft
+        slider.style.cursor = 'grabbing'
+    }
+
+    const dragging = (e) => {
+        if(!isDragStart) return;
+        e.preventDefault()
+        isDragging = true;
+        slider.classList.add("dragging-slider")
+        positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+        slider.scrollLeft = prevScrollLeft - positionDiff;
+        slider.style.cursor = 'grabbing'
+        showHideIcons();
+    }
+
+    const dragStop = () => {
+        isDragStart = false;
+        slider.classList.remove("dragging-slider")
+        slider.style.cursor = 'grab'
+
+        if(!isDragging) return;
+        isDragging = false;
+        autoSlide();
+    }
+
+    slider.addEventListener("mousedown", dragStart)
+    slider.addEventListener("touchstart", dragStart)
+
+    slider.addEventListener("mousemove", dragging)
+    slider.addEventListener("touchmove", dragging)
+
+    slider.addEventListener("mouseup", dragStop)
+    slider.addEventListener("mouseleave", dragStop)
+    slider.addEventListener("touchend", dragStop)
 });
